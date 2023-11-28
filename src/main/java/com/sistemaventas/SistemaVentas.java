3acaf43f4384d4f4cf25ec3e24ac1447ca6096be
Facundo <facundovs84@hotmail.com.ar>
2023-11-28 15:54:09 -0300

    Creada pantalla Login

--- src/main/java/com/sistemaventas/SistemaVentas.java
+++ src/main/java/com/sistemaventas/SistemaVentas.java
@@ -1,8 +1,12 @@
 package com.sistemaventas;
 
+import com.sistemaventas.view.Login;
+
 public class SistemaVentas {
 
     public static void main(String[] args) {
-        
+        Login pantallaLogin = new Login();
+        pantallaLogin.setVisible(true);
+        pantallaLogin.setLocationRelativeTo(null);
     }
 }
